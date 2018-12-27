b6e4091dabb9ad1259b79652201d2ac652649586
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2018-12-28 03:21:41 +0900

    fix template bug

--- src/posts/template.php
+++ src/posts/template.php
@@ -11,7 +11,7 @@
 <div class="wp-block-advanced-posts-block-posts <?php echo esc_attr( $class_name ); ?>">
 	<?php if ( $query->have_posts() ) : ?>
 		<?php while ( $query->have_posts() ) : ?>
-			<?php the_post(); ?>
+			<?php $query->the_post(); ?>
 			<article class="">
 				<h4 class="">
 					<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
