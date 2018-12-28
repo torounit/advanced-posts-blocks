b647f9aa0e769b065c5907d70b186e19baf6cdb5
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2018-12-28 14:22:15 +0900

    add jest

--- tests/js/sample.test.js
+++ tests/js/sample.test.js
@@ -1,5 +1,5 @@
-const sum = ( a, b) => a + b;
+const sum = ( a, b ) => a + b;
 
-test('adds 1 + 2 to equal 3', () => {
-	expect(sum(1, 2)).toBe(3);
-});
+test( 'adds 1 + 2 to equal 3', () => {
+	expect( sum( 1, 2 ) ).toBe( 3 );
+} );
