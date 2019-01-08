61edd8ca0fb9f4ba0393f13cd5202ea74be02f3d
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2019-01-09 00:14:18 +0900

    fix build script

--- bin/build.sh
+++ bin/build.sh
@@ -2,6 +2,6 @@
 
 npm install
 npm run build
+
 sed -i -e "s/nightly/${TRAVIS_TAG}/" $(basename $TRAVIS_REPO_SLUG).php
 sed -i -e "s/nightly/${TRAVIS_TAG}/" readme.txt
-sed -i -e "s/nightly/${TRAVIS_TAG}/" package.json
