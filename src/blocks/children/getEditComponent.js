73d2a29a7a16b648e53ddaefae431a076051600f
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2019-11-10 01:38:19 +0900

    use SFC

--- src/blocks/children/getEditComponent.js
+++ src/blocks/children/getEditComponent.js
@@ -1,7 +1,7 @@
 /**
  * WordPress dependencies
  */
-import { Component, Fragment } from '@wordpress/element';
+import { Fragment } from '@wordpress/element';
 import {
 	PanelBody,
 	Placeholder,
@@ -27,99 +27,96 @@ import {
 import { buildTermsTree } from '../../util/terms';
 
 const getEditComponent = ( blockName, blockTitle ) => {
-	return class extends Component {
-		render() {
-			const { className, attributes, setAttributes, posts, children, postId, selectedPostType, postTypes } = this.props;
-			const { order, orderBy, postsToShow } = attributes;
-			const labels = selectedPostType.labels || {};
+	return ( { className, attributes, setAttributes, posts, children, postId, selectedPostType, postTypes } ) => {
+		const { order, orderBy, postsToShow } = attributes;
+		const labels = selectedPostType.labels || {};
 
-			const title = __( 'Query setting', 'advanced-posts-blocks' );
+		const title = __( 'Query setting', 'advanced-posts-blocks' );
 
-			const PostTypeControls = (
-				<SelectControl
-					label={ __( 'Post Type', 'advanced-posts-blocks' ) }
-					value={ selectedPostType.slug }
-					options={ postTypes.map( ( type ) => ( { label: type.name, value: type.slug } ) ) }
-					onChange={ ( postType ) => {
-						setAttributes( { postType } );
-					} }
-				/>
-			);
-			const pagesTree = buildTermsTree( posts.map( ( item ) => ( {
-				id: item.id,
-				parent: item.parent,
-				name: item.title.raw ? item.title.raw : `#${ item.id } ${ __( '(no title)' ) }`,
-			} ) ) );//.filter( ( item ) => ( item.children.length ) );
+		const PostTypeControls = (
+			<SelectControl
+				label={ __( 'Post Type', 'advanced-posts-blocks' ) }
+				value={ selectedPostType.slug }
+				options={ postTypes.map( ( type ) => ( { label: type.name, value: type.slug } ) ) }
+				onChange={ ( postType ) => {
+					setAttributes( { postType } );
+				} }
+			/>
+		);
+		const pagesTree = buildTermsTree( posts.map( ( item ) => ( {
+			id: item.id,
+			parent: item.parent,
+			name: item.title.raw ? item.title.raw : `#${ item.id } ${ __( '(no title)' ) }`,
+		} ) ) );//.filter( ( item ) => ( item.children.length ) );
 
-			const fillWithChildren = ( terms ) => {
-				return terms.map( ( term ) => {
-					if ( term.children.length > 0 ) {
-						return {
-							...term,
-							children: fillWithChildren( term.children ),
-						};
-					}
-					return null;
-				} ).filter( ( term ) => term );
-			};
+		const fillWithChildren = ( terms ) => {
+			return terms.map( ( term ) => {
+				if ( term.children.length > 0 ) {
+					return {
+						...term,
+						children: fillWithChildren( term.children ),
+					};
+				}
+				return null;
+			} ).filter( ( term ) => term );
+		};
 
-			const ParentControls = (
-				<TreeSelect
-					label={ labels.parent_item_colon }
-					noOptionLabel={ `${ __( '(This Page)', 'advanced-posts-blocks' ) }` }
-					tree={ fillWithChildren( pagesTree ) }
-					selectedId={ postId }
-					onChange={ ( value ) => {
-						setAttributes( { postId: value ? parseInt( value ) : undefined } );
-					} }
-				/>
-			);
+		const ParentControls = (
+			<TreeSelect
+				label={ labels.parent_item_colon }
+				noOptionLabel={ `${ __( '(This Page)', 'advanced-posts-blocks' ) }` }
+				tree={ fillWithChildren( pagesTree ) }
+				selectedId={ postId }
+				onChange={ ( value ) => {
+					setAttributes( { postId: value ? parseInt( value ) : undefined } );
+				} }
+			/>
+		);
 
-			const inspectorControls = (
-				<InspectorControls>
-					<PanelBody title={ title }>
-						<QueryControls
-							{ ...{ order, orderBy } }
-							numberOfItems={ postsToShow }
-							onOrderChange={ ( value ) => setAttributes( { order: value } ) }
-							onOrderByChange={ ( value ) => setAttributes( { orderBy: value } ) }
-							onNumberOfItemsChange={ ( value ) => setAttributes( { postsToShow: value } ) }
-						/>
-						{ PostTypeControls }
-						{ ParentControls }
-					</PanelBody>
-				</InspectorControls>
-			);
-			const hasPosts = Array.isArray( children ) && children.length;
-			if ( ! hasPosts ) {
-				return (
-					<Fragment>
-						{ inspectorControls }
-						<Placeholder
-							icon="admin-post"
-							label={ blockTitle }
-						>
-							{ ! Array.isArray( children ) ?
-								<Spinner /> : labels.not_found
-							}
-						</Placeholder>
-					</Fragment>
-				);
-			}
-
+		const inspectorControls = (
+			<InspectorControls>
+				<PanelBody title={ title }>
+					<QueryControls
+						{ ...{ order, orderBy } }
+						numberOfItems={ postsToShow }
+						onOrderChange={ ( value ) => setAttributes( { order: value } ) }
+						onOrderByChange={ ( value ) => setAttributes( { orderBy: value } ) }
+						onNumberOfItemsChange={ ( value ) => setAttributes( { postsToShow: value } ) }
+					/>
+					{ PostTypeControls }
+					{ ParentControls }
+				</PanelBody>
+			</InspectorControls>
+		);
+		const hasPosts = Array.isArray( children ) && children.length;
+		if ( ! hasPosts ) {
 			return (
 				<Fragment>
 					{ inspectorControls }
-					<Disabled>
-						<ServerSideRender
-							className={ className }
-							block={ blockName }
-							attributes={ attributes }
-						/>
-					</Disabled>
+					<Placeholder
+						icon="admin-post"
+						label={ blockTitle }
+					>
+						{ ! Array.isArray( children ) ?
+							<Spinner /> : labels.not_found
+						}
+					</Placeholder>
 				</Fragment>
 			);
 		}
+
+		return (
+			<Fragment>
+				{ inspectorControls }
+				<Disabled>
+					<ServerSideRender
+						className={ className }
+						block={ blockName }
+						attributes={ attributes }
+					/>
+				</Disabled>
+			</Fragment>
+		);
 	};
 };
 
--- src/blocks/post/getEditComponent.js
+++ src/blocks/post/getEditComponent.js
@@ -1,7 +1,7 @@
 /**
  * WordPress dependencies
  */
-import { Component, Fragment } from '@wordpress/element';
+import { Fragment } from '@wordpress/element';
 import {
 	PanelBody,
 	Placeholder,
@@ -16,99 +16,77 @@ import {
 } from '@wordpress/block-editor';
 
 const getEditComponent = ( blockName, blockTitle ) => {
-	return class extends Component {
-		constructor() {
-			super( ...arguments );
-			this.state = {
-				isTaxonomiesLoaded: false,
-			};
-		}
+	return ( { className, attributes, setAttributes, posts, selectedPostType, postTypes } ) => {
+		const { postId } = attributes;
 
-		async componentDidMount() {
-			this.isStillMounted = true;
-		}
+		const PostTypeControls = (
+			<SelectControl
+				label={ __( 'Post Type', 'advanced-posts-blocks' ) }
+				value={ selectedPostType.slug }
+				options={ postTypes.map( ( type ) => ( { label: type.name, value: type.slug } ) ) }
+				onChange={ ( postType ) => {
+					setAttributes( { postId: undefined } );
+					setAttributes( { postType } );
+				} }
+			/>
+		);
+		const PostControls = (
+			<SelectControl
+				label={ __( 'Post' ) }
+				value={ postId }
+				options={ [
+					{
+						value: '',
+						label: __( 'Select Post', 'advanced-posts-blocks' ),
+					},
+					...posts.map( ( post ) => ( {
+						label: post.title.rendered,
+						value: post.id,
+					} ) ),
+				] }
+				onChange={ ( value ) => {
+					setAttributes( { postId: value ? parseInt( value ) : undefined } );
+				} }
+			/>
+		);
 
-		async componentDidUpdate() {
+		const title = __( 'Query setting', 'advanced-posts-blocks' );
 
-		}
+		const inspectorControls = (
+			<InspectorControls>
+				<PanelBody title={ title }>
+					{ PostTypeControls }
+					{ PostControls }
+				</PanelBody>
+			</InspectorControls>
+		);
 
-		componentWillUnmount() {
-			this.isStillMounted = false;
-		}
-
-		render() {
-			const { className, attributes, setAttributes, posts, selectedPostType, postTypes } = this.props;
-			const { postId } = attributes;
-
-			const PostTypeControls = (
-				<SelectControl
-					label={ __( 'Post Type', 'advanced-posts-blocks' ) }
-					value={ selectedPostType.slug }
-					options={ postTypes.map( ( type ) => ( { label: type.name, value: type.slug } ) ) }
-					onChange={ ( postType ) => {
-						setAttributes( { postId: undefined } );
-						setAttributes( { postType } );
-					} }
-				/>
-			);
-			const PostControls = (
-				<SelectControl
-					label={ __( 'Post' ) }
-					value={ postId }
-					options={ [
-						{
-							value: '',
-							label: __( 'Select Post', 'advanced-posts-blocks' ),
-						},
-						...posts.map( ( post ) => ( {
-							label: post.title.rendered,
-							value: post.id,
-						} ) ),
-					] }
-					onChange={ ( value ) => {
-						setAttributes( { postId: value ? parseInt( value ) : undefined } );
-					} }
-				/>
-			);
-
-			const title = __( 'Query setting', 'advanced-posts-blocks' );
-
-			const inspectorControls = (
-				<InspectorControls>
-					<PanelBody title={ title }>
-						{ PostTypeControls }
-						{ PostControls }
-					</PanelBody>
-				</InspectorControls>
-			);
-
-			if ( ! postId ) {
-				return (
-					<Fragment>
-						{ inspectorControls }
-						<Placeholder
-							icon="admin-post"
-							label={ blockTitle }
-						>
-							{ __( 'Post Not Found.', 'advanced-posts-blocks' ) }
-						</Placeholder>
-					</Fragment>
-				);
-			}
-
+		if ( ! postId ) {
 			return (
 				<Fragment>
 					{ inspectorControls }
-					<Disabled>
-						<ServerSideRender
-							className={ className }
-							block={ blockName }
-							attributes={ attributes }
-						/>
-					</Disabled>
+					<Placeholder
+						icon="admin-post"
+						label={ blockTitle }
+					>
+						{ __( 'Post Not Found.', 'advanced-posts-blocks' ) }
+					</Placeholder>
 				</Fragment>
 			);
 		}
+
+		return (
+			<Fragment>
+				{ inspectorControls }
+				<Disabled>
+					<ServerSideRender
+						className={ className }
+						block={ blockName }
+						attributes={ attributes }
+					/>
+				</Disabled>
+			</Fragment>
+		);
 	};
 };
 
