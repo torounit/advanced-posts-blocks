10ed47a70f2c0da5eacac638da2166966c18ad8d
Hiroshi Urabe <mail@torounit.com>
2020-01-17 12:49:21 +0900

    eslint fix

--- webpack.config.js
+++ webpack.config.js
@@ -7,13 +7,13 @@ const getEntries = () => {
 	const entries = {};
 	glob.sync( '*/index.js', {
 		cwd: srcDir,
-	} ).map( ( key ) => {
-		entries[ 'blocks/'+key.replace( '/index.js', '/index' ) ] = path.resolve( srcDir, key );
+	} ).forEach( ( key ) => {
+		entries[ 'blocks/' + key.replace( '/index.js', '/index' ) ] = path.resolve( srcDir, key );
 	} );
 	return entries;
 };
 
-const entry =  getEntries();
+const entry = getEntries();
 module.exports = {
 	...defaultConfig,
 	entry,
