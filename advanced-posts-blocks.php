98f4b3b1f52aa1295258d3688960b2ee93bb5002
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2020-07-24 04:07:27 +0900

    Release 0.8.1

--- advanced-posts-blocks.php
+++ advanced-posts-blocks.php
@@ -7,7 +7,7 @@
  * Author URI:      https://torounit.com
  * Text Domain:     advanced-posts-blocks
  * Domain Path:     /languages
- * Version: 0.8.1-rc
+ * Version: 0.8.1
  *
  * @package         Advanced_Posts_Blocks
  */
