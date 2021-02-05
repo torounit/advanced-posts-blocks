82dbc95336b23764543b69a7f6bd7d50a01686ed
Hiroshi Urabe <mail@torounit.com>
2021-02-05 23:34:47 +0900

    Release 2.1.0-rc.1

--- advanced-posts-blocks.php
+++ advanced-posts-blocks.php
@@ -7,7 +7,7 @@
  * Author URI:      https://torounit.com
  * Text Domain:     advanced-posts-blocks
  * Domain Path:     /languages
- * Version: 2.0.0
+ * Version: 2.1.0-rc.1
  *
  * @package         Advanced_Posts_Blocks
  */
